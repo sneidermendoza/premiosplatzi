c5d3fa845e6da0422f5d61e6bfeeb2f7f7a40601
sneider mendoza <mariasol0304@gmail.com>
2022-02-15 19:38:37 -0500

    progreso de la app

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index')
+    #example : /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    #example : /polls/5/
+    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
+    #example : /polls/5/results/
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    #example : /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
